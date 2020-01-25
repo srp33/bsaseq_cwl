be2b00f6408de4e41bbdf726fa16eb0e9a259099
srp33 <stephen_piccolo@byu.edu>
2020-01-24 22:38:37 -0700

    Renamed files. Added download workflow.

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-cwl-runner --outdir FASTQ_Trimmed bsaseq_trim.cwl trim_ent256.yml
+#cwl-runner --outdir FASTQ_Trimmed sickle_trim.cwl trim_ent256.yml
+cwl-runner --outdir Genome download.cwl b73_download.yml
