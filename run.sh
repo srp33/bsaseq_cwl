edeb2c9e11567889ecfba891ed2353f0b2e33c94
srp33 <stephen_piccolo@byu.edu>
2020-01-25 07:41:02 -0700

    Added gunzip

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 #cwl-runner --outdir FASTQ_Trimmed sickle_trim.cwl trim_ent256.yml
-cwl-runner --outdir Genome download.cwl b73_download.yml
+#cwl-runner --outdir Genome download.cwl b73_download.yml
+cwl-runner --outdir Genome gunzip.cwl b73_gunzip.yml
